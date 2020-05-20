4df60165d832e6ec9fee4f3ba6bcc0232277900d
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-05-20 08:53:52 +0530

    code for 0 precision corrected

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ Math.avg=(args,precision=0)=>{
     sum=sum+args[i];
   }
   let average=sum/args.length;
-  return Number((average).toFixed(precision));;
+  return precision===0?Number(average):Number((average).toFixed(precision));;
 }
 Math.qAvg=(args,precision=0)=>{
   let sum=0;
@@ -15,7 +15,7 @@ Math.qAvg=(args,precision=0)=>{
     sum=sum+args[i]*args[i];
   }
   let average=Math.sqrt((sum)/args.length);
-  return Number((average).toFixed(precision));;
+  return precision===0?Number(average):Number((average).toFixed(precision));
 }
 Math.hAvg=(args,precision=0)=>{
   let sum=0;
@@ -23,7 +23,7 @@ Math.hAvg=(args,precision=0)=>{
     sum=sum+Math.pow(args[i],-1);
   }
   let average=Math.pow((sum)/args.length,-1);
-  return Number((average).toFixed(precision));;
+  return precision===0?Number(average):Number((average).toFixed(precision));
 }
 Math.gAvg=(args,precision=0)=>{
   let product=1;
@@ -31,7 +31,7 @@ Math.gAvg=(args,precision=0)=>{
     product=product*args[i];
   }
   let average=Math.pow(product,Math.pow(args.length,-1));
-  return Number((average).toFixed(precision));;
+  return precision===0?Number(average):Number((average).toFixed(precision));
 }
 
 console.log(Math.avg([1,2,3])===2);
@@ -39,3 +39,4 @@ console.log(Math.avg([1,2,3.5],2)===2.17);
 console.log(Math.qAvg([3, 5, 7], 1)===5.3);
 console.log(Math.hAvg([3, 5, 7], 1)===4.4);
 console.log(Math.gAvg([3, 5, 7], 1)===4.7);
+console.log(Math.hAvg([1, 23, 456, 7890])===3.8248304676450755);
